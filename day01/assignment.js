be7b0befc54e98e061df56a889aed19dbf6760c2
SmiJa <SmiJa@users.noreply.github.com>
2016-08-09 22:00:06 -0700

    Add files via upload

---
+++ day01/assignment.js
@@ -0,0 +1,13 @@
+/*
+  Jason Smith
+  8/8/1026
+  Intro to Javascirpt
+*/
+
+var a = 12;
+var b = 0x1e;
+var sum = a + b;
+var d = "Answer equals: ";
+
+var answer = d + c;
+console.log(answer);
---
+++ day01/script.js
@@ -0,0 +1,22 @@
+var name ="Jason";
+var a = 5;
+var b = 6;
+var c = a + b;
+
+var e = true;
+var f = false;
+var g;
+
+var nothing = null;
+var something = undefined;
+
+var arr = ["Jason", 28, true];
+
+var car = {
+  make: "Dodge",
+  model: "Dakota",
+  year: 2007
+};
+
+
+console.log(car.model);
