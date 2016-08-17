6f155fb07b7103dc0153cc2c4529efdea44bb20d
SmiJa <SmiJa@users.noreply.github.com>
2016-08-16 22:26:15 -0700

    Add files via upload

---
+++ day03/script.js
@@ -0,0 +1,42 @@
+/*var str = 'Jason';
+console.log(str.trim());*/
+
+/*var randNum = Math.floor((Math.random() * 10) + 1);
+
+console.log(randNum);
+*/
+
+
+/*var toDay = new Date;
+var month = toDay.getMonth();
+var day = toDay.getDay();
+var year = toDay.getFullYear();*/
+
+/*switch (month) {
+  case 7:
+    month = "August";
+    break;
+}
+
+var fullDate = month + "/" + day + "/" + year;
+
+console.log(fullDate);*/
+
+
+
+/*var userFirst = prompt("What is your first name?");
+var userLast = prompt("What is your last name?");
+var userBday = prompt("What is your full written birthday?");
+var userColor = prompt("What is your favorite color?");
+
+function newFunc(uF, uL, uB, uC) {
+  var fullName = uF + " " + uL;
+  var birthDate = uB.split(" ");
+  var fullBirthDate = birthDate[0] + " " + birthDate[1] + " " + birthDate[2];
+
+
+  console.log(fullName + "\n" + fullBirthDate + "\n"+ uC);
+}
+
+newFunc(userFirst, userLast, userBday, userColor);
+*/
