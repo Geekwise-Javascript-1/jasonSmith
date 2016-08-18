61215bce2feeb3876fca87230391f11f8ad07fef
SmiJa <SmiJa@users.noreply.github.com>
2016-08-17 18:04:44 -0700

    Add files via upload

--- day03/assignment.js
+++ day03/assignment.js
@@ -5,18 +5,6 @@ var name = prompt("Enter your first name");
 // Capitalize function
 // n = name
 function capitalize(n) {
-  /*// Convert n into lower case
-  n = n.toLowerCase();
-
-  // Remove the first letter from n
-  var subN = n.slice(1, n.length);
-
-  // Isolate and capitalize first letter of n
-  var firstLetter = n.charAt(0).toUpperCase();
-
-  // Return capitalized firsrt letter to sliced copy of n
-  return firstLetter + subN;*/
-
   rturn n.charAt(0).toUpperCase() + n.substr(1, n.length).toLowerCase();
 }
 
